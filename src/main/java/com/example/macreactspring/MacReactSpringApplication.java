55c4eee49de0911d8f309ea8dee2e42220c52569
jaehoonyoun <jocback@gmail.com>
2021-12-16 21:59:15 +0900

    init source

---
+++ src/main/java/com/example/macreactspring/MacReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.macreactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacReactSpringApplication.class, args);
+    }
+
+}
